f4c8f34323d077020103909bd0267bb83a5a7608
Ahmed T. Eladl <ahmed.eladl@rub.de>
2025-02-22 00:19:37 +0100

    added a config file to define aliases in it

---
+++ src/config/aliascraft.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Alias Definitions
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify an array of alias definitions. Each alias should
+    | be defined as a key with an array value that has an "action" (callable)
+    | and optional "options" such as group and expected args.
+    |
+    | Example:
+    |   'deploy' => [
+    |       'action'  => function() {
+    |           // Your deployment logic...
+    |       },
+    |       'options' => [
+    |           'group' => 'deployment',
+    |           'args'  => []  // list expected arguments, if any
+    |       ],
+    |   ],
+    |
+    */
+    'aliases' => [
+        // 'deploy' => [...],
+    ],
+];
